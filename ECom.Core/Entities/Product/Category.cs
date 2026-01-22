7fbab672cbcf2e7feeb205ef760b07ce0e541000
Rabeamohamed <113738302+Rabeamohamed@users.noreply.github.com>
2026-01-22 17:15:22 +0200

    Add project files.

---
+++ ECom.Core/Entities/Product/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECom.Core.Entities.Product
+{
+    public class Category : BaseEntity<int>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<Product> Products { get; set; } = new HashSet<Product>();
+    }
+}
