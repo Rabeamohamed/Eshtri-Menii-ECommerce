ed00ee63411a6310e76a4c4c96a3b2e70ad8477c
Rabeamohamed <113738302+Rabeamohamed@users.noreply.github.com>
2026-01-24 11:25:55 +0200

    Create Base Controller to add the Commens between all Controllers , and Create Category Contoller with get all endpoint to retrive all categories in system

---
+++ ECom.API/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using ECom.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECom.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        protected readonly IUnitOfWork work;
+        public BaseController(IUnitOfWork work)
+        {
+            this.work = work;
+        }
+    }
+    
+}
---
+++ ECom.API/Controllers/CategoriesController.cs
@@ -0,0 +1,34 @@
+﻿using ECom.Core.Interfaces;
+using ECom.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECom.API.Controllers
+{
+
+    public class CategoriesController : BaseController
+    {
+        public CategoriesController(IUnitOfWork work) : base(work)
+        {
+        }
+
+        [HttpGet("get-all")]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            try
+            {
+                var categories = await work.CategoryRepository.GetAllAsync();
+                if (categories is null)
+                {
+                    return BadRequest("No categories found.");
+                }
+                return Ok(categories);
+            }
+            catch (Exception)
+            {
+
+                return BadRequest("Something went wrong while retrieving categories.");
+            }
+        }
+    }
+}
