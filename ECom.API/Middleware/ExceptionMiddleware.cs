b29f1fdacca874ded67f28ef66f6aece4ae424a2
Rabeamohamed <113738302+Rabeamohamed@users.noreply.github.com>
2026-01-31 10:36:09 +0200

    Create Class ApiExceptions and ExceptionMiddleware to apply Exception Handling Middeware

---
+++ ECom.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using ECom.API.Helper;
+using System.Net;
+using System.Text.Json;
+
+namespace ECom.API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+                var response = new ApiExceptions(context.Response.StatusCode, ex.Message,ex.StackTrace);
+                var json = JsonSerializer.Serialize(response);
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
