493c41ba6d8d290fac2b86eb715875b7a7e20279
Rabeamohamed <113738302+Rabeamohamed@users.noreply.github.com>
2026-01-31 10:49:48 +0200

    Increase security and mantain the Resposnse of Exception handler based on Environment if it Development display Stack Trace if not hide it.

--- ECom.API/Middleware/ExceptionMiddleware.cs
+++ ECom.API/Middleware/ExceptionMiddleware.cs
@@ -7,10 +7,12 @@ namespace ECom.API.Middleware
     public class ExceptionMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly IHostEnvironment _environment;
 
-        public ExceptionMiddleware(RequestDelegate next)
+        public ExceptionMiddleware(RequestDelegate next, IHostEnvironment environment)
         {
             _next = next;
+            _environment = environment;
         }
 
         public async Task InvokeAsync(HttpContext context)
@@ -24,8 +26,12 @@ namespace ECom.API.Middleware
 
                 context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
                 context.Response.ContentType = "application/json";
-                var response = new ApiExceptions(context.Response.StatusCode, ex.Message,ex.StackTrace);
+
+                var response = _environment.IsDevelopment()?
+                    new ApiExceptions(context.Response.StatusCode, ex.Message,ex.StackTrace)
+                        : new ApiExceptions(context.Response.StatusCode, ex.Message);
                 var json = JsonSerializer.Serialize(response);
+
                 await context.Response.WriteAsync(json);
             }
         }
