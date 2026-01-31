93364a3c1e59605fd8b1cf17da0a99e62ae8a897
Rabeamohamed <113738302+Rabeamohamed@users.noreply.github.com>
2026-01-31 09:23:52 +0200

    Create BugController for Handling Exceptions as a Global Custom Error Handler Middleware

---
+++ ECom.API/Controllers/BugController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using ECom.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECom.API.Controllers
+{
+    public class BugController : BaseController
+    {
+        public BugController(IUnitOfWork work, IMapper mapper) : base(work, mapper)
+        {
+        }
+
+        [HttpGet("not-found")]
+        
+        public async Task<ActionResult> GetNotFound()
+        {
+            var category = await work.CategoryRepository.GetByIdAsync(42);
+            if (category == null) return NotFound();
+            return Ok(category);
+        }
+
+        [HttpGet("server-error")]
+        public async Task<ActionResult> GetServerError()
+        {
+            var category = await work.CategoryRepository.GetByIdAsync(42);
+            category.Name = "";
+            return Ok(category);
+        }
+
+        [HttpGet("bad-request")]
+
+        public async Task<ActionResult> GetBadRequest()
+        {
+            return BadRequest();
+        }
+
+
+        [HttpGet("bad-request/{Id}")]
+        public async Task<ActionResult> GetBadRequest(int Id)
+        {
+            return Ok();
+        }
+
+    }
+}
