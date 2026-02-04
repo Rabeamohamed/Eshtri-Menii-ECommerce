6c2cf153d32b13324c7c9ac28d456bd4aa9307e5
Rabeamohamed <113738302+Rabeamohamed@users.noreply.github.com>
2026-02-04 17:23:36 +0200

    Create the IRepository of CustomerBasket with the Segniture of 3 Main Methods  GetBasketAsync, UpdateBasketAsync and DeleteBasketAsync

---
+++ ECom.Core/Interfaces/ICustomerBasketRepository.cs
@@ -0,0 +1,16 @@
+﻿using ECom.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECom.Core.Interfaces
+{
+    public interface ICustomerBasketRepository
+    {
+        Task<CustomerBasket> GetBasketAsync(int id); // basketId
+        Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket);
+        Task<bool> DeleteBasketAsync(int id); // basketId
+    }
+}
