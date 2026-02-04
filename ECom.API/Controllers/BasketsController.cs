23d35e53ffc34f8c6a91d21e8544a78bf49d9243
Rabeamohamed <113738302+Rabeamohamed@users.noreply.github.com>
2026-02-04 20:09:09 +0200

    Creeate BasketController and implement GetBasketItem by BasketId and UpdateBasket and DeleteBaskerItem

---
+++ ECom.API/Controllers/BasketsController.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using ECom.API.Helper;
+using ECom.Core.Entities;
+using ECom.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+namespace ECom.API.Controllers
+{
+   
+    public class BasketsController : BaseController
+    {
+        public BasketsController(IUnitOfWork work, IMapper mapper) : base(work, mapper)
+        {
+        }
+        [HttpGet("get-basket-item/{id}")]
+        public async Task<IActionResult> GetBasketItem(string id)
+        {
+            var basket = await work.CustomerBasketRepository.GetBasketAsync(id);
+            if (basket == null) return NotFound();
+            return Ok(basket);
+        }
+        [HttpPost("update-basket")]
+        public async Task<IActionResult> UpdateBasketItem( CustomerBasket basket)
+        {
+            var updatedBasket = await work.CustomerBasketRepository.UpdateBasketAsync(basket);
+            return Ok(updatedBasket);
+        }
+        [HttpDelete("delete-basket-item/{id}")]
+        public async Task<IActionResult> DeleteBasketItem( string id)
+        {
+            var deleted = await work.CustomerBasketRepository.DeleteBasketAsync(id);
+            return deleted? Ok(new ResponseAPI(200, "Item Deleted Successfully")) 
+                : BadRequest(new ResponseAPI(400));
+        }
+    }
+}
