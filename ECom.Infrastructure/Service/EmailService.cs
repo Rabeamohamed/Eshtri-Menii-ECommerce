085c6da6a99fa3788dbd7188854c79cddb7690ae
Rabeamohamed <113738302+Rabeamohamed@users.noreply.github.com>
2026-02-08 11:23:57 +0200

    Implement the EmailService Logic

--- ECom.Infrastructure/Service/EmailService.cs
+++ ECom.Infrastructure/Service/EmailService.cs
@@ -1,5 +1,7 @@
 ﻿using ECom.Core.DTO.Auth;
 using ECom.Core.Services;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +14,46 @@ namespace ECom.Infrastructure.Service
     {
         // Using SMTP [Simple Mail Transfer Protocol] for sending emails, using Gmail as an example, you can use any email provider that supports SMTP
         // there are other libraries like MailKit or System.Net.Mail
-        public Task SendEmailAsync(EmailDto emailDto)
+
+        private readonly IConfiguration _configuration;
+        public EmailService(IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            _configuration = configuration;
+        }
+        public async Task SendEmailAsync(EmailDto emailDto)
+        {
+            MimeMessage message = new ();
+            message.From.Add (new MailboxAddress ("Eshtri-Menii",_configuration["EmailSettings:From"]));
+            message.Subject = emailDto.Subject;
+            message.To.Add (new MailboxAddress (emailDto.To, emailDto.To));
+            message.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = emailDto.Content
+            };
+            using (var smtp = new MailKit.Net.Smtp.SmtpClient())
+            {
+                try
+                {
+                    await smtp.ConnectAsync(_configuration["EmailSettings:Smtp"],
+                        int.Parse(_configuration["EmailSettings:Port"]),true);
+
+                    await smtp.AuthenticateAsync(_configuration["EmailSettings:UserName"],
+                        _configuration["EmailSettings:Password"]);
+
+                    await smtp.SendAsync(message);
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+                finally
+                {
+                    //await smtp.DisconnectAsync(true);
+                    smtp.DisconnectAsync(true);
+                    smtp.Dispose();
+                }
+            }
         }
     }
 }
