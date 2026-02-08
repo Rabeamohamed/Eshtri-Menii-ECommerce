2df63bbc8c2638230f005367984e70d4f0152a82
Rabeamohamed <113738302+Rabeamohamed@users.noreply.github.com>
2026-02-08 10:25:58 +0200

    Add Email service class and add settings that will used for sending email in appsetting.json

---
+++ ECom.Core/Services/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using ECom.Core.DTO.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECom.Core.Services
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(EmailDto emailDto);
+    }
+}
---
+++ ECom.Infrastructure/Service/EmailService.cs
@@ -0,0 +1,20 @@
+﻿using ECom.Core.DTO.Auth;
+using ECom.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECom.Infrastructure.Service
+{
+    public class EmailService : IEmailService
+    {
+        // Using SMTP [Simple Mail Transfer Protocol] for sending emails, using Gmail as an example, you can use any email provider that supports SMTP
+        // there are other libraries like MailKit or System.Net.Mail
+        public Task SendEmailAsync(EmailDto emailDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
