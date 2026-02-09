ee074f285491b61dc9a2ffc2c93134f8b557b323
Rabeamohamed <113738302+Rabeamohamed@users.noreply.github.com>
2026-02-09 02:48:21 +0200

    Implement GenerateToken Method with logic of Creating Token based on Claims like UserName and Email that will use in login and signup

--- ECom.Infrastructure/Service/GenerateToken.cs
+++ ECom.Infrastructure/Service/GenerateToken.cs
@@ -1,8 +1,12 @@
 ﻿using ECom.Core.DTO;
 using ECom.Core.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +14,38 @@ namespace ECom.Infrastructure.Service
 {
     public class GenerateToken : IGenerateToken
     {
-        public Task<string> GetAndGenerateToken(AppUser user)
+        private readonly IConfiguration _configuration;
+
+        public GenerateToken(IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            _configuration = configuration;
+        }
+
+        public async
+            Task<string> GetAndGenerateToken(AppUser user)
+        {
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,user.UserName),
+                new Claim(ClaimTypes.Email,user.Email)
+            };
+
+            var Security = _configuration["Token:KeyIssuer"];
+            var key = Encoding.ASCII.GetBytes(Security);
+
+            SigningCredentials credentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256);
+
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor() 
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                Issuer = _configuration["Token:Issuer"],
+                SigningCredentials = credentials,
+                NotBefore = DateTime.Now
+            };
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            var token = handler.CreateToken(tokenDescriptor);
+            return handler.WriteToken(token);
         }
     }
 }
