79e15c87db8404cd7dd1f35e546ddd32b9c9e1ef
Rabeamohamed <113738302+Rabeamohamed@users.noreply.github.com>
2026-02-10 20:01:31 +0200

    Add Orders entity to model order details

---
+++ ECom.Core/Entities/Order/Orders.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ECom.Core.Entities.Order
+{
+    public class Orders : BaseEntity<int>
+    {
+        public string BuyerEmail { get; set; }
+        public decimal SubTotal { get; set; }
+        public DateTime OrderDate { get; set; }= DateTime.Now;
+        public ShippingAddress ShippingAddress { get; set; }
+        public DeliveryMethod DeliveryMethod { get; set; }
+        public IReadOnlyList<OrderItems> OrderItems { get; set; }
+        public Status PaymentStatus { get; set; }
+    }
+
+}
