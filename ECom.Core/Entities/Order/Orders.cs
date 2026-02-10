b31e447e85598ae3e2c28d92f9735173c65fcfff
Rabeamohamed <113738302+Rabeamohamed@users.noreply.github.com>
2026-02-10 22:10:18 +0200

    Add GetTotal method that calculate the total price of the order and The Shipping Price and Add Constructor for Orders Class

--- ECom.Core/Entities/Order/Orders.cs
+++ ECom.Core/Entities/Order/Orders.cs
@@ -3,6 +3,19 @@ namespace ECom.Core.Entities.Order
 {
     public class Orders : BaseEntity<int>
     {
+        public Orders()
+        {
+            
+        }
+        public Orders(string buyerEmail, decimal subTotal, ShippingAddress shippingAddress, DeliveryMethod deliveryMethod, IReadOnlyList<OrderItems> orderItems)
+        {
+            BuyerEmail = buyerEmail;
+            SubTotal = subTotal;
+            ShippingAddress = shippingAddress;
+            DeliveryMethod = deliveryMethod;
+            OrderItems = orderItems;
+        }
+
         public string BuyerEmail { get; set; }
         public decimal SubTotal { get; set; }
         public DateTime OrderDate { get; set; }= DateTime.Now;
@@ -10,6 +23,11 @@ namespace ECom.Core.Entities.Order
         public DeliveryMethod DeliveryMethod { get; set; }
         public IReadOnlyList<OrderItems> OrderItems { get; set; }
         public PaymentStatus Status { get; set; } = PaymentStatus.Pending;
+
+        public decimal GetTotal()
+        {
+            return SubTotal + DeliveryMethod.Price;
+        }
     }
 
 }
