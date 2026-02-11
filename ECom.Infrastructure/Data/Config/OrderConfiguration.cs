d566838aee92a6e1c55cde2810bd8de81d5bc93f
Rabeamohamed <113738302+Rabeamohamed@users.noreply.github.com>
2026-02-11 15:27:52 +0200

    Add Configuration of Orders Entity add relations with orderitems class and with shipping address and Convert Enum of PaymentStatus to String

---
+++ ECom.Infrastructure/Data/Config/OrderConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using ECom.Core.Entities.Order;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECom.Infrastructure.Data.Config
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Orders>
+    {
+        public void Configure(EntityTypeBuilder<Orders> builder)
+        {
+            // Relations between Orders and Shipping Address
+            builder.OwnsOne(x=>x.ShippingAddress, // One to one Relation
+                n => { n.WithOwner(); });
+
+            //Many to Many With OrderItems
+            builder.HasMany(x => x.OrderItems).WithOne().OnDelete(DeleteBehavior.Cascade);
+
+            // Convert the Enum to String
+            builder.Property(s => s.Status).HasConversion(x => x.ToString(),
+                x => (PaymentStatus)Enum.Parse(typeof(PaymentStatus), x));
+        }
+    }
+}
