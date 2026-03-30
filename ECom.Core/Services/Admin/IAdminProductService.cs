47aab89f5eea342a3a27eb76527c9f3f97d048da
Rabie Mohamed <113738302+Rabeamohamed@users.noreply.github.com>
2026-03-30 11:57:09 +0200

    Add Methods in Interface IAdminProductService

--- ECom.Core/Services/Admin/IAdminProductService.cs
+++ ECom.Core/Services/Admin/IAdminProductService.cs
@@ -1,6 +1,15 @@
-﻿namespace ECom.Core.Services.Admin
+﻿using ECom.Core.DTO.Product;
+using ECom.Core.Sharing;
+
+namespace ECom.Core.Services.Admin
 {
     public interface IAdminProductService
     {
+        Task <IReadOnlyList<ProductDto>> GetAllProductsAsync(ProductParams productParams);
+        Task<int> GetTotalCount();
+        Task<ProductDto> GetProductByIdAsync(int id);
+        Task<ResponseAPI> CreateProductAsync(AddProductDto dto);
+        Task<ResponseAPI> UpdateProductAsync(UpdateProductDto dto);
+        Task<ResponseAPI> DeleteProductAsync(int id);
     }
 }
