0a4b44d8c7dbf564b62e9dc3a0ca45700f8bf66e
Rabie Mohamed <113738302+Rabeamohamed@users.noreply.github.com>
2026-03-30 23:49:37 +0200

    Add AdminOrderService

--- ECom.Core/Entities/Order/OrderItems.cs
+++ ECom.Core/Entities/Order/OrderItems.cs
@@ -20,5 +20,6 @@
         public string ProductName { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
+        public virtual Orders Order { get; set; }
     }
 }
--- ECom.Infrastructure/Service/Admin/AdminOrderService.cs
+++ ECom.Infrastructure/Service/Admin/AdminOrderService.cs
@@ -1,7 +1,29 @@
-﻿using ECom.Core.Services.Admin;
+﻿using ECom.Core.DTO.Order;
+using ECom.Core.Entities.Order;
+using ECom.Core.Services.Admin;
+using ECom.Core.Sharing;
 namespace ECom.Infrastructure.Service.Admin
 {
-    public class AdminOrderService: IAdminOrderService
+    public class AdminOrderService : IAdminOrderService
     {
+        public Task<IReadOnlyList<OrderToReturnDto>> GetAllOrdersAsync(PaymentStatus? status = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderToReturnDto> GetOrderByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Dictionary<string, int>> GetOrdersCountByStatusAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseAPI> UpdateOrderStatusAsync(int orderId, PaymentStatus status)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
